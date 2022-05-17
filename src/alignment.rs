72c4898fe66c2e49cf46797a969d86d57d99804f
Mateusz Gienieczko <mat@gienieczko.com>
2022-05-17 15:25:08 +0200

    feat: added `alignment::TwoTo<N>`

--- src/alignment.rs
+++ src/alignment.rs
@@ -15,6 +15,18 @@ pub unsafe trait Alignment {
     fn size() -> usize;
 }
 
+/// Alignment to $2^N$. All acceptable alignments can be derived
+/// from this alignment, for example 64-byte alignment is simply [`TwoTo<6>`].
+///
+/// # Examples
+/// ```rust
+/// use aligners::alignment::{self, Alignment};
+///
+/// assert_eq!(64, alignment::TwoTo::<6>::size());
+/// ```
+#[derive(Debug)]
+pub struct TwoTo<const N: u32> {}
+
 /// Alignment to 1 byte, so no special alignment &ndash; every slice is always one-byte-aligned.
 ///
 /// # Examples
@@ -23,8 +35,37 @@ pub unsafe trait Alignment {
 ///
 /// assert_eq!(1, alignment::One::size());
 /// ```
-#[derive(Debug)]
-pub struct One {}
+pub type One = TwoTo<0>;
+
+/// Alignment to 2 bytes, same as [`u16`]/[`i16`].
+///
+/// # Examples
+/// ```rust
+/// use aligners::alignment::{self, Alignment};
+///
+/// assert_eq!(2, alignment::Two::size());
+/// ```
+pub type Two = TwoTo<1>;
+
+/// Alignment to 4 bytes, same as [`u32`]/[`i32`].
+///
+/// # Examples
+/// ```rust
+/// use aligners::alignment::{self, Alignment};
+///
+/// assert_eq!(4, alignment::Four::size());
+/// ```
+pub type Four = TwoTo<2>;
+
+/// Alignment to 8 bytes, same as [`u64`]/[`i64`].
+///
+/// # Examples
+/// ```rust
+/// use aligners::alignment::{self, Alignment};
+///
+/// assert_eq!(8, alignment::Eight::size());
+/// ```
+pub type Eight = TwoTo<3>;
 
 /// Alignment to page boundary.
 ///
@@ -42,11 +83,11 @@ pub struct One {}
 pub struct Page {}
 
 // SAFETY:
-// One is a constant power of two.
-unsafe impl Alignment for One {
+// 2^N is a power of two (duh).
+unsafe impl<const N: u32> Alignment for TwoTo<N> {
     #[inline(always)]
     fn size() -> usize {
-        1
+        2usize.pow(N)
     }
 }
 
